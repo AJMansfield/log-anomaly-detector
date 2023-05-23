aadbeeb05f05892ca361a2770703c5d7b5a8d126
Anson Mansfield <amansfield@mantaro.com>
2023-05-23 11:34:36 -0400

    remove trace

--- anomaly_detector/storage/es_storage.py
+++ anomaly_detector/storage/es_storage.py
@@ -48,9 +48,6 @@ class ESStorage:
             es_args["client_key"] = os.path.join(self.config.ES_CERT_DIR, "es.key")
         else:
             _LOGGER.warning("Conecting to ElasticSearch without authentication.")
-        
-        import pdb
-        pdb.set_trace()
 
         self.es = Elasticsearch(
             self.config.ES_ENDPOINT,
